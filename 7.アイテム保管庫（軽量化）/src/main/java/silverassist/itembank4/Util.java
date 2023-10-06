21658494fc032de224801b491b79200fb7b01916
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-10-06 21:43:26 +0900

    最初のコミット

---
+++ 7.アイテム保管庫（軽量化）/src/main/java/silverassist/itembank4/Util.java
@@ -0,0 +1,15 @@
+package silverassist.itembank4;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Util {
+    public static ItemStack createItem(Material mate, String name){
+        ItemStack item = new ItemStack(mate);
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setDisplayName(name);
+        item.setItemMeta(itemMeta);
+        return item;
+    }
+}
